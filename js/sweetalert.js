5536b9d1620b67efd049a40333da814fbe19a31c
ricpar123 <ricpar123@gmail.com>
2026-01-07 18:46:51 -0300

    agregado cambio.css

---
+++ js/sweetalert.js
@@ -0,0 +1,11 @@
+
+Swal.fire({
+    title: "<strong> Informes o Leer QR</strong",
+    icon: "question",
+    html: `Deseas leer <a href="../vistas/qrcode.html" autofocus>codigo QR</a>
+     o ir a <a href="../vistas/informe.html" autofocus>Informe de Servicio</a>`,
+    showCloseButton: true,
+    showCancelButton: true,
+    focusConfir: false,
+    
+});
